a0b879db03a1a2794bf6f48a90b9ac419b1f0c98
seo-jio <lio8625@naver.com>
2021-05-24 21:07:20 +0900

    서지오 DataBase, DbOpenHelper, DTO class 추가

---
+++ app/src/main/java/com/professionalandroid/apps/account_book_start/DataBase/DTO.java
@@ -0,0 +1,109 @@
+package com.professionalandroid.apps.account_book_start.DataBase;
+/*
+*   Record에 관련된 정보를 가지고 있는 객체
+*   RecyclerView의 Item 객체로 활용하기위해 부가적인
+*   total, id, week , month, year등의 인스턴트 변수를 추갛였다.
+ */
+public class DTO {
+    private String content;
+    private String income   = "0";
+    private String expends  = "0";
+    private String date;
+
+
+
+
+    private int total = 0;
+    private int id = 0;
+
+    private int week = -1;
+    private int month = -1;
+    private int year = -1;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getExpends() {
+        return expends;
+    }
+
+    public String getIncome() {
+        return income;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setExpends(String expends) {
+        this.expends = expends;
+    }
+
+    public void setIncome(String income) {
+        this.income = income;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public DTO clone(){
+        DTO dto = new DTO();
+        dto.setId(this.getId());
+        dto.setContent(this.getContent());
+        dto.setIncome(this.getIncome());
+        dto.setExpends(this.getExpends());
+        dto.setDate(this.getDate());
+        dto.setTotal(this.getTotal());
+        dto.setYear(this.getYear());
+        dto.setMonth(this.getMonth());
+        dto.setWeek(this.getWeek());
+        return dto;
+    }
+}
---
+++ app/src/main/java/com/professionalandroid/apps/account_book_start/DataBase/DbOpenHelper.java
@@ -0,0 +1,104 @@
+package com.professionalandroid.apps.account_book_start.DataBase;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+   //데이터 베이스의 전체적인 정보를 담고있는 객체
+
+public class DbOpenHelper {
+
+    private static final String DATABASE_NAME = "InnerDatabase(SQLite).db";     // 데이터 베이스의 이름
+    private static final int DATABASE_VERSION = 1;                              // 데이터 베이스의 버전
+    public static SQLiteDatabase mDB;                                           // SQLITE 객체
+    private DatabaseHelper mDBHelper;                                           // DbOpenHelper 객체
+    private Context mCtx;                                                       // DbOpenHelper를 오픈할 Context
+
+
+    private class DatabaseHelper extends SQLiteOpenHelper {
+        // 생성자
+        public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db){
+            db.execSQL(DataBases.CreateDB._CREATE0);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+            db.execSQL("DROP TABLE IF EXISTS "+DataBases.CreateDB._TABLENAME0);
+            onCreate(db);
+        }
+    }
+    
+    // DbOpenHelper 생성자
+    public DbOpenHelper(Context context){
+        this.mCtx = context;
+    }
+
+    // DB를 open 메소드
+    public DbOpenHelper open() throws SQLException {
+        mDBHelper = new DatabaseHelper(mCtx, DATABASE_NAME, null, DATABASE_VERSION);
+        mDB = mDBHelper.getWritableDatabase();      // 쓰기 가능한 Sqlite객체를 가져와 삽입
+        return this;
+    }
+
+    // DB를 생성
+    public void create(){
+        mDBHelper.onCreate(mDB);
+    }
+
+    // DB를 닫음
+    public void close(){
+        mDB.close();
+    }
+
+    // Record 값 Insert
+    public long insertColumn(String content,String income, String expend, String date){
+        ContentValues values = new ContentValues();
+        values.put(DataBases.CreateDB.CONTENT, content);
+        values.put(DataBases.CreateDB.INCOME, income);
+        values.put(DataBases.CreateDB.EXPEND, expend);
+        values.put(DataBases.CreateDB.DATE, date);
+        return mDB.insert(DataBases.CreateDB._TABLENAME0, null, values);
+    }
+ 
+    // Select 쿼리 실행
+    public Cursor selectColumns(){
+        return mDB.query(DataBases.CreateDB._TABLENAME0, null, null, null, null, null, null);
+    }
+
+    // 정렬 select 쿼리 실행
+    public Cursor sortColumn(String sort){
+        Cursor c = mDB.rawQuery( "SELECT * FROM account ORDER BY " + sort + ";", null);
+        return c;
+    }
+
+    // Update 쿼리 실행
+    public boolean updateColumn(long id,String content, String income, String expend, String date){
+        ContentValues values = new ContentValues();
+        values.put(DataBases.CreateDB.CONTENT, content);
+        values.put(DataBases.CreateDB.INCOME, income);
+        values.put(DataBases.CreateDB.EXPEND, expend);
+        values.put(DataBases.CreateDB.DATE, date);
+
+        return mDB.update(DataBases.CreateDB._TABLENAME0, values, "_id=" + id, null) > 0;
+    }
+
+    // 한 Record에 대하여 Delete 쿼리실행
+    public boolean deleteColumn(long id){
+        return mDB.delete(DataBases.CreateDB._TABLENAME0, "_id="+id, null) > 0;
+    }
+
+    // 모든 Record delete 쿼리 실행
+    public void deleteAllColumns() {
+        mDB.delete(DataBases.CreateDB._TABLENAME0, null, null);
+    }
+}
+
